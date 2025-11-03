8652231a2f362b0e10e62bac96a73f4df74e5a2e
felix-i4w <felix@impulse4women.org>
2025-11-03 15:35:26 -0400

    Initial commit - Flask Skelleton

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from .routes import bp as api_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api_bp)
+    return app
+
+app = create_app()
---
+++ app/config.py
@@ -0,0 +1,5 @@
+import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+WEAVIATE_URL   = os.getenv("WEAVIATE_URL", "")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY", "")
